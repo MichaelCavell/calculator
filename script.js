b7904d10b12b6a281c84e5f46b62dfc7538c0b4e
MichaelCavell <96754228+MichaelCavell@users.noreply.github.com>
2024-06-26 10:57:50 -0400

    Create a basic HTML  for calculator
    Create a basic HTML calculator with buttons for
    each digit, math operations and an “Equals” key.

--- script.js
+++ script.js
@@ -11,11 +11,6 @@ function divide(first, second) {
     return first / second
 }
 
-console.log(add(2, 3));
-console.log(subtract(2, 3));
-console.log(multiply(2, 3));
-console.log(divide(2, 3));
-
 let firstNumber;
 let operator;
 let secondNumber;
@@ -23,8 +18,3 @@ let secondNumber;
 function operate(first, operator, second) {
     return operator(first, second);
 }
-
-console.log(operate(2, add, 3))
-console.log(operate(2, subtract, 3))
-console.log(operate(2, multiply, 3))
-console.log(operate(2, add, 3))
