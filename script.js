73c78eb62c9a19986412b73796f068f49b18beee
MichaelCavell <96754228+MichaelCavell@users.noreply.github.com>
2024-06-26 16:30:49 -0400

    Make the calculator work
    Make the calculator work! Store the first number
    and second number that are input into the
    calculator, utilize the operator that the user
    selects, and then operate() on the two numbers
    when the user presses the “=” key.

--- script.js
+++ script.js
@@ -11,31 +11,97 @@ function divide(first, second) {
     return first / second
 }
 
-let firstNumber;
+let firstNumber = 0;
 let operator;
 let secondNumber;
 let displayValue = '0';
 let active = false;
+let firstNumberEntered = false
+let secondNumberEntered = false;
 
 function operate(first, operator, second) {
+    if (operator === '+') {
+        operator = add;
+    } else if (operator === '-') {
+        operator = subtract;
+    } else if (operator === '*') {
+        operator = multiply;
+    } else if (operator === '/') {
+        operator = divide;
+    }
+
     return operator(first, second);
 }
 
 const display = document.querySelector('.display');
-const numbers = document.querySelectorAll('.number')
+const numbers = document.querySelectorAll('.number');
+const operators = document.querySelectorAll('.operator');
+const ac = document.querySelector('.ac');
+const equals = document.querySelector('.equals');
 
 function updateDisplayValue(e) {
     if (active === false) {
         active = true;
         displayValue = '';
     }
+
+    if (firstNumberEntered === true && secondNumberEntered === true) {
+        display.textContent = firstNumber
+    }
     
     displayValue += e.target.innerText;
     display.textContent = displayValue;
 }
 
+function clearDisplay() {
+    active = false;
+    displayValue = '0';
+    display.textContent = displayValue;
+    firstNumberEntered = false
+    secondNumberEntered = false;
+}
+
+function updateNumbers(e) {
+    if (firstNumberEntered === false && secondNumberEntered === false) {
+        firstNumber = Number(displayValue);
+        operator = e.target.innerText;
+        firstNumberEntered = true;
+        active = false;
+    } else if (firstNumberEntered === true && secondNumberEntered === false) {
+        secondNumber = Number(displayValue);
+        secondNumberEntered = true;
+        firstNumber = operate(firstNumber, operator, secondNumber);
+        display.textContent = firstNumber;
+        operator = e.target.innerText;
+        active = false;
+    } else if (firstNumberEntered === true && secondNumberEntered === true) {
+        secondNumber = Number(displayValue);
+        firstNumber = operate(firstNumber, operator, secondNumber);
+        display.textContent = firstNumber;
+        operator = e.target.innerText;
+        active = false;
+    } 
+}
+
+ac.addEventListener('click', clearDisplay);
+
+equals.addEventListener('click', function() {
+    if (firstNumberEntered && active) {
+        secondNumber = Number(displayValue);
+        firstNumber = operate(firstNumber, operator, secondNumber);
+        display.textContent = firstNumber;
+        displayValue = firstNumber
+        secondNumber = ''
+        firstNumberEntered = false;
+    }
+})
+
 numbers.forEach((number) => {
     number.addEventListener('click', updateDisplayValue);  
+})
+
+operators.forEach((operator) => {
+    operator.addEventListener('click', updateNumbers);  
 })
 
 display.textContent = displayValue;
