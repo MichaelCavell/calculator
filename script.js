50af256c7b6fd9da0be2dcff11e758864e5b8d34
MichaelCavell <96754228+MichaelCavell@users.noreply.github.com>
2024-06-26 11:50:12 -0400

    Create the functions that populate the display
    Create the functions that populate the display
    when the number buttons are clicked.

--- script.js
+++ script.js
@@ -14,7 +14,29 @@ function divide(first, second) {
 let firstNumber;
 let operator;
 let secondNumber;
+let displayValue = '0';
+let active = false;
 
 function operate(first, operator, second) {
     return operator(first, second);
 }
+
+const display = document.querySelector('.display');
+const numbers = document.querySelectorAll('.number')
+
+function updateDisplayValue(e) {
+    if (active === false) {
+        active = true;
+        displayValue = '';
+    }
+    
+    displayValue += e.target.innerText;
+    display.textContent = displayValue;
+}
+
+numbers.forEach((number) => {
+    number.addEventListener('click', updateDisplayValue);  
+})
+
+display.textContent = displayValue;
+
